bcd3e21297a5d23a7b568d9dbe9e2a31a2a6cd25
VictiniX888 <victiniy@gmail.com>
2022-07-05 13:05:48 +0800

    Make BattleState work with the borrow checker

--- src/game_logic/battle/battle_state.rs
+++ src/game_logic/battle/battle_state.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+
+use anyhow::Context;
 use rand::{prelude::SliceRandom, thread_rng};
 
 use crate::game_logic::{character::CharacterBattle, enemy::Enemy};
@@ -6,8 +9,8 @@ use super::{battle_action::BattleAction, BattleEntity};
 
 /// Represents the state of an ongoing battle.
 pub struct BattleState {
-    pub enemies: Vec<Enemy>,
-    pub characters: Vec<CharacterBattle>,
+    pub enemies: Vec<Box<Enemy>>,
+    pub characters: Vec<Box<CharacterBattle>>,
 }
 
 impl BattleState {
@@ -15,30 +18,149 @@ impl BattleState {
     pub fn execute(
         &mut self,
         action: BattleAction,
-        source: &dyn BattleEntity,
-        targets: &mut [&mut dyn BattleEntity],
-    ) {
+        source_type: BattleEntityType,
+        source_index: usize,
+        target_type: BattleEntityType,
+        target_index: usize,
+    ) -> anyhow::Result<f64> {
         use BattleAction::*;
+
+        let source = self
+            .get_entity(source_type, source_index)
+            .with_context(|| {
+                format!(
+                    "Failed to obtain source entity of type {} and index {}",
+                    source_type, source_index
+                )
+            })?;
 
         match action {
             DealDamageSingle { multiplier } => {
-                // Only process action if target[0] exists.
-                if let [target] = targets {
-                    target.receive_damage(source.damage(multiplier));
-                }
+                let damage = source.damage(multiplier);
+                let target = self
+                    .get_entity_mut(target_type, target_index)
+                    .with_context(|| {
+                        format!(
+                            "Failed to obtain target entity of type {} and index {}",
+                            target_type, target_index
+                        )
+                    })?;
+                target.receive_damage(damage);
+                Ok(damage)
             }
 
             DealDamageRandom { multiplier } => {
-                if let Some(target) = targets.choose_mut(&mut thread_rng()) {
-                    target.receive_damage(source.damage(multiplier));
-                }
+                let damage = source.damage(multiplier);
+                let target = self.get_random_entity_mut(target_type).with_context(|| {
+                    format!(
+                        "Failed to obtain random target entity of type {}",
+                        target_type
+                    )
+                })?;
+                target.receive_damage(damage);
+                Ok(damage)
             }
 
             HealAll { multiplier } => {
-                for target in targets {
-                    target.receive_heal(source.heal(multiplier));
+                let heal = source.heal(multiplier);
+                for target in self.get_all_entities_mut(target_type) {
+                    target.receive_heal(heal);
                 }
+                Ok(heal)
             }
+        }
+    }
+
+    /// Returns a reference to a ```BattleEntity```.
+    fn get_entity(
+        &self,
+        entity_type: BattleEntityType,
+        entity_index: usize,
+    ) -> Option<&dyn BattleEntity> {
+        match entity_type {
+            BattleEntityType::CharacterBattle => self
+                .characters
+                .get(entity_index)
+                .map(|b| b.as_ref() as &dyn BattleEntity),
+
+            BattleEntityType::Enemy => self
+                .enemies
+                .get(entity_index)
+                .map(|b| b.as_ref() as &dyn BattleEntity),
+        }
+    }
+
+    /// Returns a mutable reference to a ```BattleEntity```.
+    fn get_entity_mut(
+        &mut self,
+        entity_type: BattleEntityType,
+        entity_index: usize,
+    ) -> Option<&mut dyn BattleEntity> {
+        match entity_type {
+            BattleEntityType::CharacterBattle => self
+                .characters
+                .get_mut(entity_index)
+                .map(|b| b.as_mut() as &mut dyn BattleEntity),
+
+            BattleEntityType::Enemy => self
+                .enemies
+                .get_mut(entity_index)
+                .map(|b| b.as_mut() as &mut dyn BattleEntity),
+        }
+    }
+
+    /// Returns a mutable reference to a random ```BattleEntity```.
+    fn get_random_entity_mut(
+        &mut self,
+        entity_type: BattleEntityType,
+    ) -> Option<&mut dyn BattleEntity> {
+        match entity_type {
+            BattleEntityType::CharacterBattle => self
+                .characters
+                .choose_mut(&mut thread_rng())
+                .map(|b| b.as_mut() as &mut dyn BattleEntity),
+
+            BattleEntityType::Enemy => self
+                .enemies
+                .choose_mut(&mut thread_rng())
+                .map(|b| b.as_mut() as &mut dyn BattleEntity),
+        }
+    }
+
+    /// Returns the references to all the ```BattleEntity```s on a specified sides.
+    fn get_all_entities_mut(
+        &mut self,
+        entity_type: BattleEntityType,
+    ) -> Vec<&mut dyn BattleEntity> {
+        match entity_type {
+            BattleEntityType::CharacterBattle => self
+                .characters
+                .iter_mut()
+                .map(|b| b.as_mut() as &mut dyn BattleEntity)
+                .collect(),
+
+            BattleEntityType::Enemy => self
+                .enemies
+                .iter_mut()
+                .map(|b| b.as_mut() as &mut dyn BattleEntity)
+                .collect(),
+        }
+    }
+}
+
+/// Represents the type of ```BattleEntity``` (either Character or Enemy).
+/// Used for determining which side is initiating and receiving the attack or skill.
+#[derive(Clone, Copy)]
+pub enum BattleEntityType {
+    CharacterBattle,
+    Enemy,
+}
+
+impl Display for BattleEntityType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::CharacterBattle => f.write_str("Character"),
+            Self::Enemy => f.write_str("Enemy"),
         }
     }
 }
--- src/game_logic/character/character_stats.rs
+++ src/game_logic/character/character_stats.rs
@@ -1,13 +1,13 @@
 use crate::game_logic::weapon::WeaponGrid;
 
 /// Gets the total HP of a character.
-/// Will take into account weapons in the future too.
+/// Will take into account buffs in the future too.
 pub fn get_total_hp(base_hp: f64, grid: &WeaponGrid) -> f64 {
     base_hp + grid.total_hp
 }
 
 /// Gets the total ATK of a character.
-/// Will take into account weapons in the future too.
+/// Will take into account buffs in the future too.
 pub fn get_total_atk(base_atk: f64, grid: &WeaponGrid) -> f64 {
     base_atk + grid.total_atk
 }
